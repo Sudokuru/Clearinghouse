0f2a7bd93fefeb28ae72e6114facba36b86680b8
Gregory Travis <gregorytravis711@gmail.com>
2023-11-25 12:22:14 -0500

    Updated GenerateInsert script to return SQL insert statement

--- GenerateInsert.ts
+++ GenerateInsert.ts
@@ -1,8 +1,9 @@
-# Given a puzzle board string returns a JSON object containing data on it
-# Example Usage: bun GenerateInsert.ts 439275618051896437876143592342687951185329746697451283928734165563912874714568329
+// Given a puzzle board string returns a SQL statement to insert the puzzle and associated data into the Puzzles table
+// Example Usage: bun GenerateInsert.ts 439275618051896437876143592342687951185329746697451283928734165563912874714568329
 
 import { getPuzzleData } from 'sudokuru';
 
 const board = process.argv[2];
+const data = getPuzzleData(board);
 
-console.log(getPuzzleData(board));
+console.log("INSERT INTO Puzzles(puzzle, solution, difficulty) VALUES(" + board + ", " + data.solution + ", " + data.difficulty + ");");
