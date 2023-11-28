67ae64b2c79dcbbaeba5a1d799f0960145a68886
Galla <gallant.thomas@outlook.com>
2023-11-27 20:00:36 -0500

    reverting incorrect changes

--- delete.sh
+++ delete.sh
@@ -6,9 +6,6 @@ SECONDS=0
 
 if docker rm -f sudoku-postgres ; then
 	echo -e "\033[32mPostgres Docker deleted successfully in $SECONDS seconds.\033[m"
-	# Clear out puzzles.sql
-	> puzzles.sql
-	echo -e "\033[32mCleared puzzles.sql\033[m"
 else
 	echo -e "\033[31mPostgres Docker failed to delete.\033[m"
 fi
