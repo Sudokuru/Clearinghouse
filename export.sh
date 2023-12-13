74a2dec61be819600b08c12b766a334a8420f9da
Gregory Travis <gregorytravis711@gmail.com>
2023-12-12 21:46:38 -0500

    Added to export.sh to run query and direct output to temp.txt

--- export.sh
+++ export.sh
@@ -2,3 +2,4 @@ echo "Querying the database to find the requested puzzles..."
 
 sql_query="SELECT puzzle, solution, difficulty FROM Puzzles WHERE difficulty >= %s AND difficulty <= %s LIMIT %s;"
 sql_query=$(printf "$sql_query" "$MIN_DIFFICULTY" "$MAX_DIFFICULTY" "$PUZZLE_COUNT")
+echo $sql_query | docker exec -i sudoku-postgres psql -U postgres -d postgres > temp.txt
