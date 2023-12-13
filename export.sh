d47dea32d4527fe12bd5f806ef9b2a021bbf1322
Gregory Travis <gregorytravis711@gmail.com>
2023-12-12 21:54:02 -0500

    Updated query to return values in comma separated format

--- export.sh
+++ export.sh
@@ -1,5 +1,5 @@
 echo "Querying the database to find the requested puzzles..."
 
-sql_query="SELECT puzzle, solution, difficulty FROM Puzzles WHERE difficulty >= %s AND difficulty <= %s LIMIT %s;"
+sql_query="SELECT string_agg(CONCAT(puzzle, ', ',  solution, ', ', difficulty), ', ') FROM Puzzles WHERE difficulty >= %s AND difficulty <= %s LIMIT %s;"
 sql_query=$(printf "$sql_query" "$MIN_DIFFICULTY" "$MAX_DIFFICULTY" "$PUZZLE_COUNT")
 echo $sql_query | docker exec -i sudoku-postgres psql -U postgres -d postgres > temp.txt
