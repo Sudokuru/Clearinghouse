c023cc4ac3e960c166fc130cb3dd43095d11205f
Gregory Travis <gregorytravis711@gmail.com>
2023-12-13 18:08:22 -0500

    Added loop to iterate over data for each specific puzzle and printed data by type

--- export.ts
+++ export.ts
@@ -5,6 +5,9 @@ const file = fs.readFileSync('temp2.csv', "utf-8");
 
 Papa.parse(file, {
     complete: function(result) {
-        console.log(result.data)
+	const colCount:int = 3;
+	for (let i:int = 0; i < (result.data[0].length) / 3; i++) {
+		console.log("Puzzle: " + result.data[0][i*3] + ", solution: " + result.data[0][(i*3)+1] + ", difficulty: " + result.data[0][(i*3)+2]);
+	}
   }
 })
