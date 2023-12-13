a24b42e3874c1b41b62d1f71db556f4f875106ce
Gregory Travis <gregorytravis711@gmail.com>
2023-12-13 18:15:48 -0500

    Cleaned up loop code

--- export.ts
+++ export.ts
@@ -6,8 +6,9 @@ const file = fs.readFileSync('temp2.csv', "utf-8");
 Papa.parse(file, {
     complete: function(result) {
 	const colCount:int = 3;
-	for (let i:int = 0; i < (result.data[0].length) / 3; i++) {
-		console.log("Puzzle: " + result.data[0][i*3] + ", solution: " + result.data[0][(i*3)+1] + ", difficulty: " + result.data[0][(i*3)+2]);
+	let data = result.data[0];
+	for (let i:int = 0; i < data.length; i += 3) {
+		console.log("Puzzle: " + data[i] + ", solution: " + data[i+1] + ", difficulty: " + data[i+2]);
 	}
   }
 })
