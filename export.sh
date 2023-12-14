16c46898f6e6abaf2f841771458be17e10dfe240
Gregory Travis <gregorytravis711@gmail.com>
2023-12-13 21:40:26 -0500

    Redirected output to exports.ts

--- export.sh
+++ export.sh
@@ -5,5 +5,5 @@ sql_query=$(printf "$sql_query" "$MIN_DIFFICULTY" "$MAX_DIFFICULTY" "$PUZZLE_COU
 echo $sql_query | docker exec -i sudoku-postgres psql -U postgres -d postgres > temp1.csv
 sed '1,2d' temp1.csv | sed '$d' | sed '$d' > temp2.csv
 rm temp1.csv
-bun export.ts
+bun export.ts > exports.ts
 rm temp2.csv
