c23f2cdeb0beadda8c1453368ddef7997aeaceff
Gregory Travis <gregorytravis711@gmail.com>
2023-12-13 20:08:19 -0500

    Added side length and for loop over each cell

--- export.ts
+++ export.ts
@@ -5,14 +5,19 @@ const file = fs.readFileSync('temp2.csv', "utf-8");
 
 Papa.parse(file, {
     complete: function(result) {
-	const colCount:int = 3;
+	const colCount:number = 3;
 	let data = result.data[0];
-	for (let i:int = 0; i < data.length; i += 3) {
-		let puzzle = data[i]
-		let solution = data[i+1]
-		let difficulty = data[i+2]
+	for (let i:number = 0; i < data.length; i += 3) {
+		let puzzle = data[i];
+		let sideLength = Math.sqrt(puzzle.length);
+		let solution = data[i+1];
+		let difficulty = data[i+2];
 		console.log("Puzzle: " + puzzle + ", solution: " + solution + ", difficulty: " + difficulty);
 		let obj:string = '[{"type":"classic","version":"1.0.0","selectedCell":{"r":3,"c":0},"puzzle":[';
+		for (let row:number = 0; row < sideLength; row++) {
+			for (let col:number = 0; col < sideLength; col++) {
+			}
+		}
 		console.log(obj + "\n");
 	}
   }
