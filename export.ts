b58280d12e59c0c98e3f5bbaad3512b1bd737dc1
Gregory Travis <gregorytravis711@gmail.com>
2023-12-13 19:55:50 -0500

    Started puzzle obj

--- export.ts
+++ export.ts
@@ -12,6 +12,8 @@ Papa.parse(file, {
 		let solution = data[i+1]
 		let difficulty = data[i+2]
 		console.log("Puzzle: " + puzzle + ", solution: " + solution + ", difficulty: " + difficulty);
+		let obj:string = '[{"type":"classic","version":"1.0.0","selectedCell":{"r":3,"c":0},"puzzle":[';
+		console.log(obj + "\n");
 	}
   }
 })
