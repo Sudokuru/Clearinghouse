e16a5d25447db04f964c4bed964ef93c1def8c97
Gregory Travis <gregorytravis711@gmail.com>
2023-12-14 20:18:18 -0500

    Fixed sql query so it limits correctly

--- export.sh
+++ export.sh
@@ -1,6 +1,6 @@
 echo "Querying the database to find the requested puzzles..."
 
-sql_query="SELECT string_agg(CONCAT(puzzle, ', ',  solution, ', ', difficulty), ', ') FROM Puzzles WHERE difficulty >= %s AND difficulty <= %s LIMIT %s;"
+sql_query="SELECT string_agg(CONCAT(puzzle, ', ', solution, ', ', difficulty), ', ') FROM Puzzles WHERE difficulty >= %s AND difficulty <= %s GROUP BY puzzle LIMIT %s;"
 sql_query=$(printf "$sql_query" "$MIN_DIFFICULTY" "$MAX_DIFFICULTY" "$PUZZLE_COUNT")
 echo $sql_query | docker exec -i sudoku-postgres psql -U postgres -d postgres > temp1.csv
 sed '1,2d' temp1.csv | sed '$d' | sed '$d' > temp2.csv # removes the first and last two lines
