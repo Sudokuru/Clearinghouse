d11a3c196f3e46e4cacabfa285cb6d37dc8c2e65
Gregory Travis <89822791+Gregory711@users.noreply.github.com>
2023-12-30 11:38:36 -0500

    User Runtime Input (#9)
    Added ability for export.sh to ask user for values if none provided instead of crashing

--- export.sh
+++ export.sh
@@ -1,5 +1,15 @@
 source utils.sh
 
+if [[ -z "$PUZZLE_COUNT" ]]; then
+	read -p "Enter how many puzzles you want to export: " PUZZLE_COUNT
+fi
+if [[ -z "$MIN_DIFFICULTY" ]]; then
+	read -p "Enter the minimum acceptable difficulty of puzzles you want to export: " MIN_DIFFICULTY
+fi
+if [[ -z "$MAX_DIFFICULTY" ]]; then
+	read -p "Enter the maximum acceptable difficulty of puzzles you want to export: " MAX_DIFFICULTY
+fi
+
 print_yellow "Querying the database to find the requested puzzles..."
 
 sql_query="SELECT string_agg(CONCAT(puzzle, ', ', solution, ', ', difficulty), ', ') FROM Puzzles WHERE difficulty >= %s AND difficulty <= %s GROUP BY puzzle LIMIT %s;"
