827392b79ec94366d80fd86316e73324bd896aa8
Gregory Travis <89822791+Gregory711@users.noreply.github.com>
2024-01-28 13:45:54 -0500

    Compact Puzzle Exports (#13)
    Updated export to use new compact format and added type safety.

--- export.ts
+++ export.ts
@@ -5,7 +5,8 @@ const file = fs.readFileSync('temp2.csv', "utf-8");
 
 Papa.parse(file, {
     complete: function(result) {
-	let objs = 'export const GAMES: SudokuObjectProps[] = [';
+	let objs:string = 'import { Puzzle } from \'./Puzzle.type\';\n';
+	objs += 'export const PUZZLES: Puzzle[] = [';
 	for (let puzzleIndex: number = 0; puzzleIndex < result.data.length; puzzleIndex++) {
 		const colCount:number = 3;
 		let data = result.data[puzzleIndex];
@@ -14,60 +15,11 @@ Papa.parse(file, {
 		}
 		for (let i:number = 0; i < data.length; i += colCount) {
 			let puzzle:string = data[i].trim();
-			let sideLength:number = Math.sqrt(puzzle.length);
 			let solution:string = data[i+1].trim();
 			let difficulty:string = data[i+2].trim();
 			//console.log("Puzzle: " + puzzle + ", solution: " + solution + ", difficulty: " + difficulty);
-			let obj:string = '{"type":"classic","version":"1.0.0","selectedCell":null,"puzzle":[';
-			for (let row:number = 0; row < sideLength; row++) {
-				obj += '[';
-				for (let col:number = 0; col < sideLength; col++) {
-					let type:string = "given";
-					const cellValue:string = puzzle.charAt((row*sideLength)+col);
-					if (cellValue == "0"){
-						type = "value";
-					}
-					obj += '{"type":"'
-					obj += type;
-					obj += '","entry":';
-					obj += cellValue;
-					obj += '}';
-					if (col < (sideLength - 1)) {
-						obj += ',';
-					}
-				}
-				obj += ']';
-				if (row < (sideLength - 1)) {
-					obj += ',';
-				}
-			}
-			obj += '],';
-			obj += '"puzzleSolution":';
-			obj += '[';
-			for (let row:number = 0; row < sideLength; row++) {
-				obj += '[';
-				for (let col:number = 0; col < sideLength; col++) {
-					obj += solution.charAt((row*sideLength)+col);
-					if (col < (sideLength - 1)) {
-						obj += ',';
-					}
-				}
-				obj += ']'
-				if (row < (sideLength - 1)) {
-					obj += ',';
-				}
-			}
-			obj += '],';
-			obj += '"statistics":{"difficulty":"easy","internalDifficulty":';
-			obj += difficulty;
-			obj += ',"numHintsUsed":0,"numWrongCellsPlayed":0,"score":0,"time":0},"inNoteMode":true,"actionHistory":[]}';
-			//console.log(obj + "\n");
-			objs += obj;
-			if (i < (data.length - 3)) {
-				objs += ',';
-			}
+			objs += `{"p":"${puzzle}","s":"${solution}","d":${difficulty}},`;
 		}
-		objs += ',';
 	}
 	objs = objs.slice(0, -1); // remove trailing comma
 	objs += '];';
