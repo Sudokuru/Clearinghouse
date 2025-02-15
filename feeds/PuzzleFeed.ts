f847e9008bf912d0b465d64c4addaaf18bb068f8
Gregory Travis <gregorytravis711@gmail.com>
2025-02-15 17:03:16 -0500

    No need to make interface generic

--- feeds/PuzzleFeed.ts
+++ feeds/PuzzleFeed.ts
@@ -1,6 +1,6 @@
 import { Puzzle } from "../types/Puzzle";
 
-export interface PuzzleFeed<T> {
+export interface PuzzleFeed {
     /**
      * Returns the next Puzzle in the feed or null if the feed is exhausted.
      */
