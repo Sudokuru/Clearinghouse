74a87db239d773a40df60967757b522f7875bab5
Gregory Travis <gregorytravis711@gmail.com>
2025-02-15 17:17:49 -0500

    2 space indents

--- feeds/PuzzleFeed.ts
+++ feeds/PuzzleFeed.ts
@@ -1,8 +1,8 @@
 import { Puzzle } from "../types/Puzzle";
 
 export interface PuzzleFeed {
-    /**
-     * Returns the next Puzzle in the feed or null if the feed is exhausted.
-     */
-    next(): Promise<Puzzle | null>;
+  /**
+   * Returns the next Puzzle in the feed or null if the feed is exhausted.
+   */
+  next(): Promise<Puzzle | null>;
 }
