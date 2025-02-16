769528b20fdb6d4ca8eb54740138425603492ff9
Gregory Travis <gregorytravis711@gmail.com>
2025-02-16 13:00:04 -0500

    Simplified txt feed

--- feeds/TxtPuzzleFeed.ts
+++ feeds/TxtPuzzleFeed.ts
@@ -4,7 +4,6 @@ import { getIterator, PuzzleFeed } from "./PuzzleFeed";
 
 export class TxtPuzzleFeed implements PuzzleFeed {
   private rl: AsyncIterator<string>;
-  private lineNum: number = 0;
 
   constructor(txtFilePath: string) {
     this.rl = getIterator(txtFilePath);
@@ -16,20 +15,14 @@ export class TxtPuzzleFeed implements PuzzleFeed {
         return null;
     }
 
-    this.lineNum++;
-    const line = data.value;
-    if (!line.trim()) {
+    const line = data.value.trim();
+    if (!line) {
       // Skip empty lines by recursively calling next().
       return this.next();
     }
 
-    const values = line.split(',').map(val => val.trim());
-    if (values.length > 1) {
-        log(`Line ${this.lineNum} has more fields than expected.`, COLORS.RED);
-    }
-
     return {
-        key: 'unsolved:' + values[0],
+        key: 'unsolved:' + line,
         data: {} as PuzzleData
     } as Puzzle;
   }
