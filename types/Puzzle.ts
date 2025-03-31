3e3624d9cec41f3347d70f95ffebeee86775dac9
Gregory Travis <gregorytravis711@gmail.com>
2025-03-30 22:08:09 -0400

    Fixed zod schema to fix test pulling presolved data back out of redis

--- types/Puzzle.ts
+++ types/Puzzle.ts
@@ -4,23 +4,23 @@ export const PuzzleDataSchema = z.object({
   // solution: 81 character puzzle solution string
   solution: z.string(),
   // difficulty: Integer difficulty metric as determined by Sudokuru npm package
-  difficulty: z.number(),
+  difficulty: z.coerce.number(),
   // A drill is when you can search and find exactly one occurrence of a strategy at
   // a particular point in a puzzle
   // Last occurrence of each drill when solving puzzle using nextStep function
   // from Sudokuru package repeatedly until their are that many cells filled in
   // i.e. 80 is last move
-  // If drill never occurrs in puzzle then value is set to -1
-  obvious_single_drill: z.number(),
-  hidden_single_drill: z.number(),
-  obvious_pair_drill: z.number(),
-  hidden_pair_drill: z.number(),
-  pointing_pair_drill: z.number(),
-  obvious_triplet_drill: z.number(),
-  hidden_triplet_drill: z.number(),
-  pointing_triplet_drill: z.number(),
-  obvious_quadruplet_drill: z.number(),
-  hidden_quadruplet_drill: z.number()
+  // If drill never occurs in puzzle then value is set to -1
+  obvious_single_drill: z.coerce.number(),
+  hidden_single_drill: z.coerce.number(),
+  obvious_pair_drill: z.coerce.number(),
+  hidden_pair_drill: z.coerce.number(),
+  pointing_pair_drill: z.coerce.number(),
+  obvious_triplet_drill: z.coerce.number(),
+  hidden_triplet_drill: z.coerce.number(),
+  pointing_triplet_drill: z.coerce.number(),
+  obvious_quadruplet_drill: z.coerce.number(),
+  hidden_quadruplet_drill: z.coerce.number()
 });
 
 export type PuzzleData = z.infer<typeof PuzzleDataSchema>;
