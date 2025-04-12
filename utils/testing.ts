74c5e1e2a90d70b730e7b09cd34fc3d9fabc28ca
Gregory Travis <gregorytravis711@gmail.com>
2025-04-12 11:54:53 -0400

    More types

--- utils/testing.ts
+++ utils/testing.ts
@@ -1,3 +1,4 @@
+import { RedisClientType } from "redis";
 import { PuzzleData, PuzzleDataSchema, PuzzleKey } from "../types/Puzzle";
 import { COLORS, log } from "./logs";
 import { clearRedis, getPuzzleDataFromRedis, stopRedis } from "./redis";
@@ -6,7 +7,7 @@ import { z } from "zod";
 /**
  * Does Redis cleanup 
  */
-export async function cleanup(redisClient): Promise<void> {
+export async function cleanup(redisClient: RedisClientType): Promise<void> {
   await clearRedis();
   await redisClient.quit();
   await stopRedis();
@@ -20,7 +21,7 @@ export async function cleanup(redisClient): Promise<void> {
 /**
  * Does cleanup, logs test failure with given message, and exits early. 
  */
-export async function cleanupAndExit(message: string, redisClient): Promise<void> {
+export async function cleanupAndExit(message: string, redisClient: RedisClientType): Promise<void> {
   log("❌ Test Failed: " + message, COLORS.RED);
   await cleanup(redisClient);
   process.exit(1);
@@ -31,7 +32,7 @@ export async function cleanupAndExit(message: string, redisClient): Promise<void
  * If a substring is not found in output then test failure is loggged, output is logged,
  * redis is cleared, and test process is exited. Otherwise, clears Redis and exits the program.
  */
-export async function assertOutputContains(output: string, contained: string[], name: string, redisClient): Promise<void> {
+export async function assertOutputContains(output: string, contained: string[], name: string, redisClient: RedisClientType): Promise<void> {
   for (const substring of contained) {
     if (!output.includes(substring)) {
       log(`Captured logs: ${output}`);
@@ -43,7 +44,7 @@ export async function assertOutputContains(output: string, contained: string[], 
 /**
  * Verifies puzzle is contained in Redis with given data else exits
  */
-export async function assertRedisContainsPuzzleData(redisClient, puzzle: string, puzzleData) {
+export async function assertRedisContainsPuzzleData(redisClient: RedisClientType, puzzle: string, puzzleData) {
   const presolvedActualData = await getPuzzleDataFromRedis(redisClient, puzzle);
   if (presolvedActualData === null) {
     await cleanupAndExit("Failed to get presolved puzzle out of Redis after running start.ts", redisClient);
@@ -60,7 +61,7 @@ export async function assertRedisContainsPuzzleData(redisClient, puzzle: string,
 /**
  * Verifies string occurs exactly once in provided string array
  */
-export async function assertStringInArrayExactlyOnce(redisClient, strings: string[], string: string) {
+export async function assertStringInArrayExactlyOnce(redisClient: RedisClientType, strings: string[], string: string) {
   if (strings.filter(str => str === string).length !== 1) {
     await cleanupAndExit(`'${string}' did not occur exactly once in '${strings}'.`, redisClient);
   }
