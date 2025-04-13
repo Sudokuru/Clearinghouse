3e3096012c8858ad0ff4a6650631ed5acaa787ea
Gregory Travis <gregorytravis711@gmail.com>
2025-04-12 21:30:23 -0400

    Just use docker in ci

--- utils/redis.ts
+++ utils/redis.ts
@@ -19,11 +19,6 @@ export const QUIT_REDIS_MSG = "Redis connection closed. Exiting.";
  * Returns true if the container started or restarted successfully, otherwise false.
  */
 export async function startRedis(): Promise<boolean> {
-  // GitHub Action creates Redis Service
-  if (process.env.CI === "true") {
-    return true;
-  }
-
   log("Starting Redis Docker...", COLORS.YELLOW);
   const startTime = Date.now();
 
@@ -64,11 +59,6 @@ export async function startRedis(): Promise<boolean> {
  * Returns true if the container stopped successfully, otherwise false.
  */
 export async function stopRedis(): Promise<boolean> {
-  // GitHub Action creates Redis Service
-  if (process.env.CI === "true") {
-    return true;
-  }
-
   log("Stopping Redis Docker...", COLORS.YELLOW);
   const startTime = Date.now();
 
