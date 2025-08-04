dd2e52b5b78deb0a11f9053fd497ffb2dc78acfe
Gregory Travis <gregorytravis711@gmail.com>
2025-08-03 20:32:16 -0400

    Removed time limit option from drill ingest

--- ingest_drills.ts
+++ ingest_drills.ts
@@ -9,13 +9,11 @@ import { promptUserToConfirmValues } from "./utils/logs";
 
 // Assign environment variables to variables with fallback defaults.
 const BASE: number = 10;
-const generateTimeLimit: number = parseInt(process.env.GENERATE_TIME_LIMIT ?? "60", BASE);
 const maxDrillsPerStrategy: number = parseInt(process.env.MAX_DRILLS_PER_STRATEGY ?? "5000", BASE);
 const solvedDrillFile: string = process.env.SOLVED_DRILL_FILE ?? DEFAULT_SOLVED_DRILLS_FILE;
 const solvedPuzzleFile: string | null = process.env.SOLVED_PUZZLE_FILE ?? null;
 
 const config = {
-  "Generate Time Limit": generateTimeLimit,
   "Max Drills Per Strategy": maxDrillsPerStrategy,
   "Solved Puzzle File": solvedPuzzleFile,
   "Solved Drill File": solvedDrillFile
